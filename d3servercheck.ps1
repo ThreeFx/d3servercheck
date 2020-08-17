d9a8db5689da88793234cb607eb67e79a1a48540
Ben Fiedler <git@services.bfiedler.ch>
2020-08-17 02:15:55 +0200

    Update d3servercheck.ps1

--- d3servercheck.ps1
+++ d3servercheck.ps1
@@ -1,14 +1,49 @@
+function Write-Server-Result($tcpconns) {
+    if (!$tcpconns) {
+        return
+    }
+
+    foreach ($tcpconn in $tcpconns) {
+        Write-Host "$($tcpconn.RemoteAddress) (" -NoNewline
+        if ($is_good_ip.ContainsKey($tcpconn.RemoteAddress)) {
+            if ($is_good_ip[$tcpconn.RemoteAddress]) {
+                Write-Host "GOOD" -NoNewline
+            } else {
+                Write-Host "BAD" -NoNewline
+            }
+        } else {
+            Write-Host "UNKNOWN" -NoNewline
+        }
+
+        Write-Host ") " -NoNewline
+    }
+}
+
 Write-Host "Starting d3servercheck"
 
 $csv = Import-Csv -Path servers.csv
 
-$hm = @{}
+$is_good_ip = @{}
 
 foreach ($ip in $csv) {
-    $hm.Add($ip.server, $ip.status -eq "good")
+    $is_good_ip.Add($ip.server, $ip.status -eq "good")
 }
 
-$ips = $csv.server
+$blizzard_server_ips_32 = @()
+$blizzard_server_ips_33 = @()
+$known_ips = $csv.server
+for ($j=0; $j -lt 256; $j++) {
+    $ip_32 = "37.244.32.$($j)"
+    $ip_33 = "37.244.33.$($j)"
+    if (!$is_good_ip.ContainsKey($ip_32)) {
+        $blizzard_server_ips_32 += $ip_32
+    }
+
+    if (!$is_good_ip.ContainsKey($ip_33)) {
+        $blizzard_server_ips_33 += $ip_33
+    }
+   
+}
 
 Write-Host "Read servers"
 Write-Host "Starting checks"
@@ -16,38 +51,20 @@ Write-Host "Starting checks"
 while ($true) {
     Start-Sleep -s 5
 
-    $tcpconns = Get-NetTCPConnection -RemoteAddress $ips -EA SilentlyContinue
+    $tcpconns_0 = Get-NetTCPConnection -RemoteAddress $known_ips -EA SilentlyContinue
+    $tcpconns_1 = Get-NetTCPConnection -RemoteAddress $blizzard_server_ips_32 -EA SilentlyContinue
+    $tcpconns_2 = Get-NetTCPConnection -RemoteAddress $blizzard_server_ips_33 -EA SilentlyContinue
 
-    if (!$tcpconns) {
-        Write-Host "no TCP connections to known servers found"
+    if (!$tcpconns_0 -and !$tcpconns_1 -and !$tcpconns_2) {
+        Write-Host "no connection to Blizzard game servers detected"
         continue;
     }
 
-    $hasgood = $false
-    $hasbad = $false
-    $isunknown = $tcpconns.Length -eq 0
+    Write-Host "connections: " -NoNewline
 
-    foreach ($tcpconn in $tcpconns) {
-        if ($hm[$tcpconn.RemoteAddress]) {
-            $hasgood = $true
-        } else {
-            $hasbad = $true
-        }
-    }
+    Write-Server-Result($tcpconns_0)
+    Write-Server-Result($tcpconns_1)
+    Write-Server-Result($tcpconns_2)
 
-    Write-Host "connected to " -NoNewline
-
-    if ($isunknown) {
-        Write-Host "UNKNOWN " -NoNewline
-    } else {
-        if ($hasgood) {
-            Write-Host "GOOD " -NoNewline
-        }
-        
-        if ($hasbad) {
-            Write-Host "BAD " -NoNewline
-        }
-    }
-
-    Write-Host "server"
+    Write-Host ""
 }
